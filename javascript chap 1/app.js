99877912c3e322d1cfbb86412f74865c3ce39837
irtazaAhmed147 <irtazaahmedk@gmail.com>
2024-08-21 17:41:57 +0500

    chap 1 done

---
+++ javascript chap 1/app.js
@@ -0,0 +1,16 @@
+// Answer no 1
+alert("Welcome!")
+
+// Answer no 2
+alert("Error! Please enter a valid password")
+
+
+// Answer no 3
+alert("Welcome to JS Land... \nHappy Coding")
+
+// Answer no 4
+alert("Welcome to JS Land...")
+alert("Happy Coding")
+
+// Answer no 5
+console.log("Hello... I can run JS through my web browser's console")
